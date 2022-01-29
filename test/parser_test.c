eef78104d9334b0aa83e3bfaa4014f094b52b0be
Nikola Tasic <ntasic7@gmail.com>
2022-01-29 17:54:34 +0100

    uncommented tests

--- test/parser_test.c
+++ test/parser_test.c
@@ -7,37 +7,37 @@ int main(void){
 	Ast_Node* first_node = NULL;
 	Parser parser;
 
-	// char* code1 = "if true {1} else {2};";
-	// lexer_lex(code1, &lexer_result);
-	// parser_new(&parser, code1);
-	// result = parser_parse(&parser, &lexer_result);
-	//
-	// first_node = result.nodes[0];
-	// assert(first_node->type == AST_IF);
-	// assert(first_node->middle->type == AST_LITERAL);
-	// assert(first_node->left->type == AST_BLOCK);
-	// assert(first_node->left->nodes[0]->type == AST_LITERAL);
-	// assert(first_node->right->type == AST_BLOCK);
-	// assert(first_node->right->nodes[0]->type == AST_LITERAL);
-	// free(lexer_result.data);
-	// parser_free(&parser);
-	//
-	// char* code2 = "if a == true {2} else {3};";
-	// lexer_lex(code2, &lexer_result);
-	// parser_new(&parser, code2);
-	// result = parser_parse(&parser, &lexer_result);
-	//
-	// first_node = result.nodes[0];
-	// assert(first_node->type == AST_IF);
-	// assert(first_node->middle->type == AST_EQUALITY);
-	// assert(first_node->middle->left->type == AST_IDENT);
-	// assert(first_node->middle->right->type == AST_LITERAL);
-	// assert(first_node->left->type == AST_BLOCK);
-	// assert(first_node->left->nodes[0]->type == AST_LITERAL);
-	// assert(first_node->right->type == AST_BLOCK);
-	// assert(first_node->right->nodes[0]->type == AST_LITERAL);
-	// free(lexer_result.data);
-	// parser_free(&parser);
+	char* code1 = "if true {1} else {2};";
+	lexer_lex(code1, &lexer_result);
+	parser_new(&parser, code1);
+	result = parser_parse(&parser, &lexer_result);
+
+	first_node = result.nodes[0];
+	assert(first_node->type == AST_IF);
+	assert(first_node->middle->type == AST_LITERAL);
+	assert(first_node->left->type == AST_BLOCK);
+	assert(first_node->left->nodes[0]->type == AST_LITERAL);
+	assert(first_node->right->type == AST_BLOCK);
+	assert(first_node->right->nodes[0]->type == AST_LITERAL);
+	free(lexer_result.data);
+	parser_free(&parser);
+
+	char* code2 = "if a == true {2} else {3};";
+	lexer_lex(code2, &lexer_result);
+	parser_new(&parser, code2);
+	result = parser_parse(&parser, &lexer_result);
+
+	first_node = result.nodes[0];
+	assert(first_node->type == AST_IF);
+	assert(first_node->middle->type == AST_EQUALITY);
+	assert(first_node->middle->left->type == AST_IDENT);
+	assert(first_node->middle->right->type == AST_LITERAL);
+	assert(first_node->left->type == AST_BLOCK);
+	assert(first_node->left->nodes[0]->type == AST_LITERAL);
+	assert(first_node->right->type == AST_BLOCK);
+	assert(first_node->right->nodes[0]->type == AST_LITERAL);
+	free(lexer_result.data);
+	parser_free(&parser);
 
 	char* code3 = "a == true { function(); }}";
 	lexer_lex(code3, &lexer_result);
